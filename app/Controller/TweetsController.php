30b7111456b5412ae80594a9d9e19e6ff108c3f4
kazuki mokuo <sp9t5ns9@icloud.com>
2019-11-24 23:00:12 +0900

    added commentout at Tweetscontroller to print phpinfo

--- app/Controller/TweetsController.php
+++ app/Controller/TweetsController.php
@@ -8,5 +8,14 @@ class TweetsController extends AppController {
 	public function index() {
 		$words = "テスト";
 		$this->set("words",$words);
+		/**
+		 * phpifoを表示するときに使う。
+		 * $this->autoRender = false;
+  	 * ob_start()；
+  	 * phpinfo();
+  	 * $html = ob_get_contents();
+  	 * ob_end_clean();
+  	 * return $html;
+		 */
 	}
 }
