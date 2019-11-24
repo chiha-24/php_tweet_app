1f30e8736fe3a3973a8df30a6c6c383806f5ecb6
kazuki mokuo <sp9t5ns9@icloud.com>
2019-11-24 22:39:56 +0900

    added tweetscontrollertest.php

---
+++ app/Test/Case/Controller/TweetsControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+App::uses('TweetsController', 'Controller');
+
+/**
+ * TweetsController Test Case
+ */
+class TweetsControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.tweet'
+	);
+
+}
