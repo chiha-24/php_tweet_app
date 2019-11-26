ca53448c93d1ae23ec14d5e24a35d9b21a0450cc
kazuki mokuo <sp9t5ns9@icloud.com>
2019-11-26 22:34:31 +0900

    画像付き投稿データの加工処理の実装
    TweetsControllerにて、特定のユーザーの最新画像付きツイートを取得後、画像urlを抽出して配列にまとめる処理を記入した。

--- app/Controller/TweetsController.php
+++ app/Controller/TweetsController.php
@@ -30,7 +30,6 @@ class TweetsController extends AppController {
 
 		// マイページURLを入力された場合の処理
 		$regexp = "/https:\/\/twitter.com\//";
-		// 正規表現でキーワード置き換え
 		if(preg_match($regexp,$keyWord)){
 			$keyWord = preg_replace($regexp,"",$keyWord);
 		}
@@ -43,7 +42,7 @@ class TweetsController extends AppController {
 		if(isset($result->errors)){
 			return false;
 		}
-		// 出ていなかった場合
+		// エラーが出てなかった場合
 		elseif (count($result) != 0){
 			$counted = count($result);
 			$this->set("counted",$counted);
@@ -60,7 +59,7 @@ class TweetsController extends AppController {
 	public function show(){
 		$this->layout = 'tweets';
 
-		// resultアクションからscreen_nameを受け取る。無い場合トップページへリダイレクト
+		// リクエストからscreen_nameを受け取る。無い場合トップページへリダイレクト
 		if(!isset($_GET["screen_name"])){
 			$this->redirect(array(
 				'controller' => 'tweets',
@@ -69,7 +68,7 @@ class TweetsController extends AppController {
 			return false;
 		}
 
-		// リクエスト末尾のscreen_nameからtwitterIDを取得
+		// リクエスト末尾からtwitterIDを取得
 		$userName = $_GET["screen_name"];
 
 		// 特定のユーザー１件の取得
@@ -102,6 +101,35 @@ class TweetsController extends AppController {
 				'action' => 'index'
 			));
 			return false;
+		}
+
+		// リクエストからユーザーIDを取得（ツイート取得に使用）
+		$userName = $_GET["screen_name"];
+
+		// 画像ツイートの取得（最新順）
+		$twitter    = $this->Tweets->twitterOAuthInitialize();
+		$imagePosts = $twitter->get('search/tweets', ["q" => "from:$userName filter:images exclude:retweets","result_type" => "recent","count" => "5", "include_entities"=>true,"tweet_mode"=>"extended"]);
+
+		// 画像ツイートが取得できた場合
+		if (count($imagePosts) != 0){
+
+		// DB保存用にレスポンスデータを加工
+		// 配列形式で保存用データを格納する
+		$allImagePosts = [];
+		foreach ($imagePosts->statuses as $post){
+
+			// 画像urlのみを配列にまとめる
+			$tweetImageUrls = [];
+			$tweetId = $post->id;
+
+			foreach ($post->entities->media as $media){
+				$tweetImageUrl = $media->media_url;
+				array_push($tweetImageUrls,$tweetImageUrl);
+			}
+			// ツイートID(key) => 画像URLの配列(value)の形で連想配列を組んで$allImagePostsに格納
+			$allImagePosts[$tweetId] = $tweetImageUrls;
+		}
+		print_r($allImagePosts);
 		}
 	}
 }
