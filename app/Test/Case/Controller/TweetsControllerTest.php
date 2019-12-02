463ea99ce99cbd2dcb380c8fc8b8507ba984a3fe
kazuki mokuo <sp9t5ns9@icloud.com>
2019-12-02 17:26:52 +0900

    DB設定の変更
    テスト用のデータベースを用意するため、docker-compose.ymlのvolumesに- ./docker/mysql:/docker-entrypoint-initdb.dの項目を追加し、twimg_testを追加で作成するコードを記入した。

---
+++ app/Test/Case/Controller/TweetsControllerTest.php
@@ -0,0 +1,54 @@
+<?php
+App::uses('TweetsController', 'Controller');
+
+/**
+ * TweetsController Test Case
+ */
+class TweetsControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.tweet'
+	);
+
+/**
+ * testIndex method
+ *
+ * @return void
+ */
+	public function testIndex() {
+		$this->markTestIncomplete('testIndex not implemented.');
+	}
+
+/**
+ * testResult method
+ *
+ * @return void
+ */
+	public function testResult() {
+		$this->markTestIncomplete('testResult not implemented.');
+	}
+
+/**
+ * testShow method
+ *
+ * @return void
+ */
+	public function testShow() {
+		$this->markTestIncomplete('testShow not implemented.');
+	}
+
+/**
+ * testTweetImage method
+ *
+ * @return void
+ */
+	public function testTweetImage() {
+		$this->markTestIncomplete('testTweetImage not implemented.');
+	}
+
+}
