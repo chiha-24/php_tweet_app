8c3dd723329bd65d90d98536f779fcd78e3b2bfb
kazuki mokuo <sp9t5ns9@icloud.com>
2019-12-03 18:34:41 +0900

    MySQL設定の変更
    コンテナの初回build時に、MySQLのコンテナイメージをupした際にinitialize_database.sh内のGRANT ALL ~がエラーを出してコンテナが起動できていなかったため、コマンドを修正した。

--- docker/mysql/initialize_database.sh
+++ docker/mysql/initialize_database.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-
-#!/bin/sh
-
-echo "CREATE DATABASE IF NOT EXISTS \`twimg_test\` ;" | "${mysql[@]}"
-echo "GRANT ALL ON \`twimg_test\`.* TO 'root'@'root_password' ;" | "${mysql[@]}"
-echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
+echo "CREATE DATABASE IF NOT EXISTS twimg_test ;" 
+echo "GRANT ALL ON twimg_test.* TO root@'％' ;" 
+echo 'FLUSH PRIVILEGES ;' 
