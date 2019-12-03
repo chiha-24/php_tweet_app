8a1008f17218f869ca51ef57685f08f243d99f07
kazuki mokuo <sp9t5ns9@icloud.com>
2019-12-03 19:09:39 +0900

    テストDBの作成エラーの解決
    initialize_database.shにて、テスト用のデータベースであるtwimg_testが上手く作られていなかったため、コードを修正した。

--- docker/mysql/initialize_database.sh
+++ docker/mysql/initialize_database.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-echo "CREATE DATABASE IF NOT EXISTS twimg_test ;" 
-echo "GRANT ALL ON twimg_test.* TO root@'％' ;" 
-echo 'FLUSH PRIVILEGES ;' 
+
+echo "CREATE DATABASE IF NOT EXISTS \`twimg_test\` ;" | "${mysql[@]}"
+echo "GRANT ALL ON \`twimg_test\`.* TO 'root'@'%' ;" | "${mysql[@]}"
+echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
